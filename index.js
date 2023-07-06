69a1be219d497cd5f34f496fc1f976e7590e9f17
jim3 <j3.casey@protonmail.com>
2023-07-06 12:58:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-app.set("view engine", "ejs");
+const path = require("path");
 
 app.use(cors());
-app.use("/public", express.static(process.cwd() + "/public"));
+app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
