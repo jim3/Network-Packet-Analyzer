128135d387c6d9e969a067ab8d1343fdb1cd24bb
jim3 <jimmy3.xyz@gmail.com>
2023-07-08 14:09:20 -0400

    Update packetAnalyzer.js

--- services/packetAnalyzer.js
+++ services/packetAnalyzer.js
@@ -2,210 +2,204 @@ const fs = require("fs/promises");
 const dotenv = require("dotenv");
 dotenv.config();
 
+// ------------------------- IP Addresses ------------------------- //
+
 class PacketAnalyzer {
     constructor() {}
     async ipAddresses(data) {
-        data = await fs.readFile(data, "utf-8");
-        data = JSON.parse(data);
-        data = Object.keys(data).map(function (key) {
-            return data[key];
-        });
+        try {
+            data = await fs.readFile(data, "utf-8");
+            data = JSON.parse(data);
+            data = Object.keys(data).map(function (key) {
+                return data[key];
+            });
 
-        const ipSet = new Set();
-        const fieldsToCheck = ["ip.src", "ip.dst"];
+            const ipSet = new Set();
+            const fieldsToCheck = ["ip.src", "ip.dst"];
 
-        data.forEach((e) => {
-            if (e._source.layers && e._source.layers.ip) {
-                const ip = e._source.layers.ip;
-                fieldsToCheck.forEach((field) => {
-                    if (ip[field]) {
-                        ipSet.add(ip[field]);
-                    }
-                });
-            }
-        });
+            data.forEach((e) => {
+                if (e._source.layers && e._source.layers.ip) {
+                    const ip = e._source.layers.ip;
+                    fieldsToCheck.forEach((field) => {
+                        if (ip[field]) {
+                            ipSet.add(ip[field]);
+                        }
+                    });
+                }
+            });
 
-        const ipAddr = Array.from(ipSet);
-        return ipAddr;
+            const ipAddr = Array.from(ipSet);
+            return ipAddr;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     // ------------------------- DNS ------------------------- //
 
     async dns(data) {
-        data = await fs.readFile(data, "utf-8");
-        data = JSON.parse(data);
-        data = Object.keys(data).map(function (key) {
-            return data[key];
-        });
-        const dnsSet = new Set();
+        try {
+            data = await fs.readFile(data, "utf-8");
+            data = JSON.parse(data);
+            data = Object.keys(data).map(function (key) {
+                return data[key];
+            });
+            const dnsSet = new Set();
 
-        data.forEach((e) => {
-            if (e._source.layers.dns) {
-                const dns = e._source.layers.dns; // grab the dns object
+            data.forEach((e) => {
+                if (e._source.layers.dns) {
+                    const dns = e._source.layers.dns; // grab the dns object
 
-                const queriesArray = Object.values(dns.Queries);
-                if (queriesArray) {
-                    queriesArray.forEach((query) => {
-                        if (query["dns.qry.name"]) {
-                            dnsSet.add(query["dns.qry.name"]);
-                        }
-                    });
-                }
-                const answersObj = dns.Answers;
-                if (answersObj) {
-                    const answersArray = Object.values(answersObj);
-                    if (answersArray) {
-                        answersArray.forEach((answer) => {
-                            if (answer["dns.resp.name"]) {
-                                dnsSet.add(answer["dns.resp.name"]);
+                    const queriesArray = Object.values(dns.Queries);
+                    if (queriesArray) {
+                        queriesArray.forEach((query) => {
+                            if (query["dns.qry.name"]) {
+                                dnsSet.add(query["dns.qry.name"]);
                             }
                         });
                     }
+                    const answersObj = dns.Answers;
+                    if (answersObj) {
+                        const answersArray = Object.values(answersObj);
+                        if (answersArray) {
+                            answersArray.forEach((answer) => {
+                                if (answer["dns.resp.name"]) {
+                                    dnsSet.add(answer["dns.resp.name"]);
+                                }
+                            });
+                        }
+                    }
                 }
-            }
-        });
+            });
 
-        const dnsArray = Array.from(dnsSet);
-        return dnsArray;
+            const dnsArray = Array.from(dnsSet);
+            return dnsArray;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     // ------------------------- HTTP ------------------------- //
 
     async http(data) {
-        data = await fs.readFile(data, "utf-8");
-        data = JSON.parse(data);
-        data = Object.keys(data).map(function (key) {
-            return data[key];
-        });
-        const httpSet = new Set();
-        const fieldsToCheck = ["http.host", "http.request.full_uri", "http.request.method", "http.user_agent"];
+        try {
+            data = await fs.readFile(data, "utf-8");
+            data = JSON.parse(data);
+            data = Object.keys(data).map(function (key) {
+                return data[key];
+            });
+            const httpSet = new Set();
+            const fieldsToCheck = ["http.host", "http.request.full_uri", "http.request.method", "http.user_agent"];
 
-        data.forEach((e) => {
-            if (e._source.layers.http) {
-                const http = e._source.layers.http; // grab the http object
-                fieldsToCheck.forEach((field) => {
-                    if (http[field]) {
-                        httpSet.add(http[field]);
-                    }
-                });
-            }
-        });
+            data.forEach((e) => {
+                if (e._source.layers.http) {
+                    const http = e._source.layers.http; // grab the http object
+                    fieldsToCheck.forEach((field) => {
+                        if (http[field]) {
+                            httpSet.add(http[field]);
+                        }
+                    });
+                }
+            });
 
-        const httpArray = Array.from(httpSet);
-        return httpArray;
+            const httpArray = Array.from(httpSet);
+            return httpArray;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     // ------------------------- MAC Addresses ------------------------- //
 
     async macAddresses(data) {
-        data = await fs.readFile(data, "utf-8");
-        data = JSON.parse(data);
-        data = Object.keys(data).map(function (key) {
-            return data[key];
-        });
+        try {
+            data = await fs.readFile(data, "utf-8");
+            data = JSON.parse(data);
+            data = Object.keys(data).map(function (key) {
+                return data[key];
+            });
 
-        const macSourceSet = new Set();
-        const macDestinationSet = new Set();
+            const macSet = new Set();
+            const fieldsToCheck = ["eth.src", "eth.dst"];
 
-        data.forEach((e) => {
-            if (e._source.layers && e._source.layers.eth) {
-                const macSource = e._source.layers.eth["eth.src"];
-                const macDestination = e._source.layers.eth["eth.dst"];
-                if (macSource) {
-                    macSourceSet.add(macSource);
+            data.forEach((e) => {
+                if (e._source.layers.eth) {
+                    const mac = e._source.layers.eth;
+                    fieldsToCheck.forEach((field) => {
+                        if (mac[field]) {
+                            macSet.add(mac[field]);
+                        }
+                    });
                 }
-                if (macDestination) {
-                    macDestinationSet.add(macDestination);
-                }
-            }
-        });
+            });
 
-        const macSourceArray = Array.from(macSourceSet);
-        const macDestinationArray = Array.from(macDestinationSet);
-
-        const macAddresses = {
-            macSource: macSourceArray,
-            macDestination: macDestinationArray,
-        };
-
-        return macAddresses;
+            const macArray = Array.from(macSet);
+            return macArray;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     // ------------------------- UDP Ports ------------------------- //
 
     async udpPorts(data) {
-        data = await fs.readFile(data, "utf-8");
-        data = JSON.parse(data);
-        data = Object.keys(data).map(function (key) {
-            return data[key];
-        });
+        try {
+            data = await fs.readFile(data, "utf-8");
+            data = JSON.parse(data);
+            data = Object.keys(data).map(function (key) {
+                return data[key];
+            });
 
-        const udpSourceSet = new Set();
-        const udpDestinationSet = new Set();
+            const udpSet = new Set();
+            const fieldsToCheck = ["udp.port"];
 
-        if (!data) {
-            throw new Error("No data provided");
+            data.forEach((e) => {
+                if (e._source.layers.udp) {
+                    const udp = e._source.layers.udp;
+                    fieldsToCheck.forEach((field) => {
+                        if (udp[field]) {
+                            udpSet.add(udp[field]);
+                        }
+                    });
+                }
+            });
+
+            const udpArray = Array.from(udpSet);
+            return udpArray;
+        } catch (error) {
+            console.log(error);
         }
-        data.forEach((e) => {
-            if (e._source.layers && e._source.layers.udp) {
-                const udpSource = e._source.layers.udp["udp.srcport"];
-                const udpDestination = e._source.layers.udp["udp.dstport"];
-
-                if (udpSource) {
-                    udpSourceSet.add(udpSource);
-                }
-
-                if (udpDestination) {
-                    udpDestinationSet.add(udpDestination);
-                }
-            }
-        });
-
-        const udpSourceArray = Array.from(udpSourceSet);
-        const udpDestinationArray = Array.from(udpDestinationSet);
-
-        const udpPorts = {
-            udpSource: udpSourceArray,
-            udpDestination: udpDestinationArray,
-        };
-
-        return udpPorts;
     }
 
     // ------------------------- TCP Ports ------------------------- //
 
     async tcpPorts(data) {
-        data = await fs.readFile(data, "utf-8");
-        data = JSON.parse(data);
-        data = Object.keys(data).map(function (key) {
-            return data[key];
-        });
+        try {
+            data = await fs.readFile(data, "utf-8");
+            data = JSON.parse(data);
+            data = Object.keys(data).map(function (key) {
+                return data[key];
+            });
 
-        const tcpSourceSet = new Set();
-        const tcpDestinationSet = new Set();
+            const tcpSet = new Set();
+            const fieldsToCheck = ["tcp.port"];
 
-        data.forEach((e) => {
-            if (e._source.layers && e._source.layers.tcp) {
-                const tcpSource = e._source.layers.tcp["tcp.srcport"];
-                const tcpDestination = e._source.layers.tcp["tcp.dstport"];
-                if (tcpSource) {
-                    tcpSourceSet.add(tcpSource);
+            data.forEach((e) => {
+                if (e._source.layers.tcp) {
+                    const tcp = e._source.layers.tcp;
+                    fieldsToCheck.forEach((field) => {
+                        if (tcp[field]) {
+                            tcpSet.add(tcp[field]);
+                        }
+                    });
                 }
-                if (tcpDestination) {
-                    tcpDestinationSet.add(tcpDestination);
-                }
-            }
-        });
+            });
 
-        const tcpSourceArray = Array.from(tcpSourceSet);
-        const tcpDestinationArray = Array.from(tcpDestinationSet);
-
-        const tcpPorts = {
-            tcpSource: tcpSourceArray,
-            tcpDestination: tcpDestinationArray,
-        };
-
-        return tcpPorts;
+            const tcpArray = Array.from(tcpSet);
+            return tcpArray;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     // ------------------------- IP Details ------------------------- //
@@ -269,7 +263,7 @@ class PacketAnalyzer {
         return ipDetails;
     }
 
-    // ------------------------------------------------------ //
+    // -------------------------------------------------- //
 
     async analyzePacketFile(filePath) {
         if (!filePath) {
@@ -279,18 +273,18 @@ class PacketAnalyzer {
             const ipAddr = await this.ipAddresses(filePath);
             const dnsArray = await this.dns(filePath);
             const httpArray = await this.http(filePath);
-            const macAddresses = await this.macAddresses(filePath);
-            const udpPorts = await this.udpPorts(filePath);
-            const tcpPorts = await this.tcpPorts(filePath);
+            const macArray = await this.macAddresses(filePath);
+            const udpArray = await this.udpPorts(filePath);
+            const tcpArray = await this.tcpPorts(filePath);
             const ipDetails = await this.ipDetails(filePath);
 
             const packetData = {
                 ipAddr,
                 dnsArray,
                 httpArray,
-                macAddresses,
-                udpPorts,
-                tcpPorts,
+                macArray,
+                udpArray,
+                tcpArray,
                 ipDetails,
             };
 
